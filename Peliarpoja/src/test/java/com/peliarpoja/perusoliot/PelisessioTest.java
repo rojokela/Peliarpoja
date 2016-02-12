caeb41b24c2c0018f5eeb48867ed9b5a0773ddb8
rojokela <roni.jokelainen@gmail.com>
2016-02-12 15:16:55 +0200

    asdf

---
+++ Peliarpoja/src/test/java/com/peliarpoja/perusoliot/PelisessioTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.peliarpoja.perusoliot;
+
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Kompuutteri
+ */
+public class PelisessioTest {
+    Pelisessio p;
+    public PelisessioTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        Pelisessio p = new Pelisessio();
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    
+    @Test
+    public void lisaaPelaajaLisaaPelaajan() {
+       
+    }
+}
